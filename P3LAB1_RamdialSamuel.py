40222d19da4c541c8256e1013b3fc3046f92e17f
Ramdials6892 <70536081+Ramdials6892@users.noreply.github.com>
2020-09-16 13:51:49 -0400

    Add files via upload

---
+++ P3LAB1_RamdialSamuel.py
@@ -0,0 +1,72 @@
+input_month = input()
+input_day = int(input())
+if input_month == "March":
+    if input_day>=20 and input_day<=31:
+        print('Spring')
+    if input_day>=1 and input_day<20:
+        print('Winter')
+    else:
+        print('Invalid')
+elif input_month == "April":
+    if input_day>=1 and input_day<=30:
+      print('Spring')
+    else:
+      print('Invalid')
+elif input_month == "May":
+    if input_day>=1 and input_day<=31:
+      print('Spring')
+    else:
+      print('Invalid')
+elif input_month == "June":
+    if input_day >=1 and input_day<=20:
+      print('Spring')
+    if input_day >20 and input_day<=30:
+      print('Summer')
+    else:
+      print('Invalid')
+elif input_month == "July":
+  if input_day>=1 and input_day<=31:
+    print('Summer')
+  else:
+    print('Invalid')
+elif input_month == "August":
+  if input_day>=1 and input_day <=31:
+    print('Summer')
+  else:
+    print('Invalid')
+elif input_month == "September":
+  if input_day>=1 and input_day <=21: 
+    print('Summer')
+  if input_day>21 and input_day<=30:
+    print('Autumn')
+  else: 
+    print('Invalid')
+elif input_month == "October":
+  if input_day>=1 and input_day<=31:
+    print('Autumn')
+  else:
+    print('Invalid')
+elif input_month == "November":
+  if input_day>=1 and input_day<=30:
+    print('Autumn')
+  else:
+    print('Invalid')
+elif input_month == "December":
+  if input_day>=1 and input_day<=20:
+    print('Autumn')
+  if input_day>20 and input_day<=31:
+    print('Winter')
+  else:
+    print('Invalid')
+elif input_month == "January":
+  if input_day>=1 and input_day<=31:
+    print('Winter')
+  else:
+    print('Invalid')
+elif input_month == "February":
+  if input_day>=1 and input_day<=29:
+    print('Winter')
+  else:
+    print('Invalid')
+else:
+  print('Invalid')
